4763fcd1a3718cdddb48c0831b980e89c2752ffc
Junting <342766475@qq.com>
2019-11-28 23:29:38 +0800

    基础语法

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: []
+    };
+  }
+
+  changeInput(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+  handleAdd() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ""
+    });
+  }
+
+  render() {
+    return (
+      // Fragment 包裹后，无需再外面包一层容器，可以返回多个子容器，渲染的时候也不会有一层包裹
+      <Fragment>
+        <div className="App">
+          {this.state.inputValue}
+          <div>
+            <input
+              type="text"
+              value={this.state.inputValue}
+              onChange={this.changeInput.bind(this)}
+            />
+            <button onClick={this.handleAdd.bind(this)}>提交</button>
+          </div>
+          <ul>
+            {this.state.list.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
