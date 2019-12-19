ad7b4439bd37b1dd6eafdb1871ab4ca071ec8c4d
Junting <342766475@qq.com>
2019-12-19 22:33:51 +0800

    react-transition-group 动画组件，实现复杂的动画效果

--- src/Animate.js
+++ src/Animate.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment} from 'react';
-import './todoList.css';
+import { CSSTransition } from 'react-transition-group';
+// import './todoList.css';
 
 class Animate extends Component {
   constructor(props) {
@@ -18,7 +19,17 @@ class Animate extends Component {
   render() {
     return (
       <Fragment>
-        <p className={ this.state.show ? 'show': 'hide' }>React</p>
+        <CSSTransition
+          in={this.state.show} // 确定动画是否启动
+          timeout={2000} // 动画持续时间
+          classNames='fade' // 类名前缀
+          appear={true} // 一出现就有动画
+          unmountOnExit // 出场动画完成后移除 DOM
+          onEntered={(el) => {el.style.color = 'green'}} // 动画组件的钩子函数
+        >
+          <p>React</p>
+          {/* <p className={ this.state.show ? 'show': 'hide' }>React</p> */}
+        </CSSTransition>
         <button onClick={this.handleToggle}>Toggle</button>
       </Fragment>
     )
