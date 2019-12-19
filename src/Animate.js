7b25dc699fac3dc39502d43ced9f81ed32c04a1c
Junting <342766475@qq.com>
2019-12-19 22:56:28 +0800

    react-transition-group 动画组件，TransitionGroup 支持多个组件动画

--- src/Animate.js
+++ src/Animate.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment} from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 // import './todoList.css';
 
 class Animate extends Component {
@@ -7,30 +7,45 @@ class Animate extends Component {
     super(props);
 
     this.state = {
-      show: true
+      show: true,
+      list: []
     }
     this.handleToggle = this.handleToggle.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
   }
 
   handleToggle() {
     this.setState(() => ({ show: !this.state.show }))
   }
+  handleAdd() {
+    this.setState((prevState) => ({ list: [...prevState.list, 'item'] }))
+  }
 
   render() {
     return (
       <Fragment>
-        <CSSTransition
-          in={this.state.show} // 确定动画是否启动
-          timeout={2000} // 动画持续时间
-          classNames='fade' // 类名前缀
-          appear={true} // 一出现就有动画
-          unmountOnExit // 出场动画完成后移除 DOM
-          onEntered={(el) => {el.style.color = 'green'}} // 动画组件的钩子函数
-        >
-          <p>React</p>
-          {/* <p className={ this.state.show ? 'show': 'hide' }>React</p> */}
-        </CSSTransition>
-        <button onClick={this.handleToggle}>Toggle</button>
+        <TransitionGroup>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition
+                  key={index}
+                  in={this.state.show} // 确定动画是否启动
+                  timeout={2000} // 动画持续时间
+                  classNames='fade' // 类名前缀
+                  appear={true} // 一出现就有动画
+                  unmountOnExit // 出场动画完成后移除 DOM
+                  onEntered={(el) => {el.style.color = 'green'}} // 动画组件的钩子函数
+                >
+                <p>{item}</p>
+                {/* <p className={ this.state.show ? 'show': 'hide' }>React</p> */}
+              </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+
+        <button onClick={this.handleAdd}>Add</button>
       </Fragment>
     )
   }
