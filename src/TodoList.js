28e7f979e40797db0ec303e82c468b77d96912f1
Junting <342766475@qq.com>
2020-03-07 10:54:36 +0800

    还原最初状态，创建一个 TodoList 组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,80 +1,10 @@
-/**
- * 容器组件（聪明组件）
- * 不负责页面渲染工作，只负责业务逻辑
- * @Author: Junting
- * @Date: 2019-12-24 22:48:57
- * @Last Modified by: Junting
- * @Last Modified time: 2020-03-03 16:17:51
- */
-import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import TodoListUI from './TodoListUI'
-import store from './store';
-import {
-  getInputChangeAction,
-  getAddItemAction,
-  getDeleteItemAction,
-  initListAction
-} from './store/actionCreator';
-import Axios from 'axios';
+import React from 'react';
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = store.getState();
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    // 当前组件订阅 store, store 有变更，就会执行 this.handleStoreChange 方法
-    store.subscribe(this.handleStoreChange);
-  }
-
-  componentDidMount() {
-    Axios.get('/api/list').then((res) => {
-      const { status, msg, data } = res.data;
-      console.log("TCL: TodoList -> componentDidMount -> msg", msg)
-      if(status === 200) {
-        const action = initListAction(data);
-        store.dispatch(action);
-      }
-    }).catch(err => {
-      console.log("TCL: TodoList -> componentDidMount -> err", err);
-    })
-  }
-
-  render () {
+class TodoList extends React.Component {
+  render() {
     return (
-      <TodoListUI
-        inputValue={ this.state.inputValue }
-        list={this.state.list}
-        handleInputChange={this.handleInputChange}
-        handleBtnClick={this.handleBtnClick}
-        handleItemDelete={this.handleItemDelete}
-      />
-    )
-  }
-
-  handleInputChange(e) {
-    const action = getInputChangeAction(e.target.value);
-    // 通知 store 变更
-    store.dispatch(action)
-  }
-
-  handleBtnClick() {
-    const action = getAddItemAction();
-    store.dispatch(action)
-  }
-
-  handleItemDelete(index) {
-    console.log("TCL: TodoList -> handleItemDelete -> index", index)
-    const action = getDeleteItemAction(index);
-    store.dispatch(action);
-  }
-
-  handleStoreChange() {
-    this.setState(store.getState());
+      <div></div>
+    );
   }
 }
 
--- src/store/actionTypes.js
+++
@@ -1,7 +0,0 @@
-// 针对 action.type 抽离出来成常量或变量，利于排查问题
-// 页面里 action.type 等比一个字符串的话，字符串写错了的话很难排查出问题
-
-export const CHANGE_INPUT_VALUE = 'change_input_value';
-export const ADD_TODO_ITEM = 'add_todo_item';
-export const DELETE_TODO_ITEM = 'delete_todo_item';
-export const INIT_LIST_ACTION = 'init_list_action';
