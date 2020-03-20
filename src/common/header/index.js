2d630d39826d1db6ea9562709f1af69bd07a9193
Junting <342766475@qq.com>
2020-03-20 15:38:34 +0800

    将 Header 组件改写成为无状态组件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -12,58 +12,43 @@ import {
   Button
 } from './style';
 
-class Header extends React.Component {
-
-  handleInputFocus() {
-    this.setState({
-      focused: true
-    });
-  }
-
-  handleInputBlur() {
-    this.setState({
-      focused: false
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <HeaderWrapper>
-          <Logo/>
-          <Nav>
-            <NavItem className="left active">首页</NavItem>
-            <NavItem className="left">下载 APP</NavItem>
-            <NavItem className="right">登录</NavItem>
-            <NavItem className="right">
-              <i className="iconfont icon-Aa"></i>
-            </NavItem>
-            <SearchWrapper>
-              <CSSTransition
-                in={this.props.focused}
-                timeout={200}
-                classNames="slide"
-              >
-                <NavSearch
-                  className={ this.props.focused ? 'focused' : '' }
-                  onFocus={this.props.handleInputFocus}
-                  onBlur={this.props.handleInputBlur}
-                ></NavSearch>
-              </CSSTransition>
-              <i className={ this.props.focused ? 'iconfont icon-Magnifier focused' : 'iconfont icon-Magnifier' }></i>
-            </SearchWrapper>
-          </Nav>
-          <Addition>
-            <Button className="registered">注册</Button>
-            <Button className="writting">
-              <i className="iconfont icon-write"></i> 写文章
-            </Button>
-          </Addition>
-        </HeaderWrapper>
-      </div>
-    )
-  }
-}
+const Header = (props) => {
+  return (
+    <div>
+      <HeaderWrapper>
+        <Logo/>
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载 APP</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">
+            <i className="iconfont icon-Aa"></i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={props.focused}
+              timeout={200}
+              classNames="slide"
+            >
+              <NavSearch
+                className={ props.focused ? 'focused' : '' }
+                onFocus={props.handleInputFocus}
+                onBlur={props.handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <i className={ props.focused ? 'iconfont icon-Magnifier focused' : 'iconfont icon-Magnifier' }></i>
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className="registered">注册</Button>
+          <Button className="writting">
+            <i className="iconfont icon-write"></i> 写文章
+          </Button>
+        </Addition>
+      </HeaderWrapper>
+    </div>
+  )
+};
 
 const mapStateToProps = (state) => {
   return {
