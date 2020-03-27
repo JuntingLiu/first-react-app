19ca5cf10c6c22f1964c2c695e98a4f79c293928
Junting <342766475@qq.com>
2020-03-27 16:27:27 +0800

    react router 初尝试

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store';
 import Header from './common/header/index';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Header ></Header>
-      hello world
-    </Provider>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Header />
+        <Router>
+          <Route path="/" exact render={() => <div>Home</div>} />
+          {/* 访问 /detail 的时候 “/” 也会被匹配上， 使用 exact 属性精准匹配 */}
+          <Route path="/detail" exact render={() => <div>Detail</div>}/>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
